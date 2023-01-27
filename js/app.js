5202754debf10671316336d0381e0982e902c847
gultar <sacha-olivier.dulac@hotmail.com>
2023-01-26 20:07:36 -0500

    made some styling changes

--- js/app.js
+++ js/app.js
@@ -81,6 +81,8 @@ const makeTerminalWindow = async () =>{
         mount:termContainer,
         x: "center",
         y: "center",
+        height:"500",
+        width:"400",
         onclose:()=>{
             termContainer.innerHTML = ""
         } 
